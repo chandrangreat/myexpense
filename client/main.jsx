188c84525b9a5c63296b41b5b7cf0c5a72de0f58
chandrangreat <chandrangreatfree@gmail.com>
2016-09-11 12:18:43 +0530

    some updates

--- client/main.jsx
+++ client/main.jsx
@@ -6,6 +6,6 @@ import App from '../imports/ui/App.jsx';
 import Navbar from '../imports/ui/Navbar.jsx';
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById('target-place'));
   render(<Navbar />, document.getElementById('target-header'));
+  render(<App />, document.getElementById('target-place'));
 });
