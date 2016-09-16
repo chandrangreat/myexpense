a503d1eef4ce0db03c5a53ae68c383a5f4175b7a
chandrangreat <chandrangreatfree@gmail.com>
2016-09-16 22:46:33 +0530

    Clean URLs enabled in react-router

--- client/main.jsx
+++ client/main.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import App from '../imports/ui/App.jsx';
 import Login from '../imports/ui/Login.jsx';
 import Welcome from '../imports/ui/Welcome.jsx';
@@ -9,7 +9,7 @@ import Welcome from '../imports/ui/Welcome.jsx';
 
 
 Meteor.startup(() => {
-  render(<Router history={hashHistory}>
+  render(<Router history={browserHistory}>
     <Route path="/" component={App} >
       <IndexRoute component={Welcome}/>
       <Route path="/login" component={Login}/>
