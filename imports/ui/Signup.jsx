6815ca7faf8f7e0e0808e7e964de51551e33de92
chandrangreat <chandrangreatfree@gmail.com>
2016-10-05 22:16:36 +0530

    Added first name and last name to signup form

--- imports/ui/Signup.jsx
+++ imports/ui/Signup.jsx
@@ -10,8 +10,11 @@ export default class Signup extends Component{
         event.preventDefault();
       const uname = ReactDOM.findDOMNode(this.refs.uname).value.trim();
       const password = ReactDOM.findDOMNode(this.refs.password).value.trim();
-     alert(uname);
+     
     }
+
+    
+
     render(){
         return(
             <div className="container signup-container">
@@ -19,6 +22,8 @@ export default class Signup extends Component{
                     <div className="col-6 centered">
                     <h3 className="text-center">Signup</h3>
                      <form ref="signup" className="signup" onSubmit={ this.handleSubmit.bind(this) }>
+                        <input type="text" ref="fname" name="fname" placeholder="First Name" />
+                        <input type="text" ref="lname" name="lname" placeholder="Last Name" />
                         <input type="text"ref="uname" name="uname" placeholder="Username"/>
                         <input type="password" ref="password" name="password" placeholder="Password" />
                         <input type="submit" />
