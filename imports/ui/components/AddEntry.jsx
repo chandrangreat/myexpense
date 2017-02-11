65b203a12b0f7e1dc52d5923d12a3df264ab74d1
chandrangreat <chandrangreatfree@gmail.com>
2017-02-11 22:52:40 +0530

    Changed headernavigation

--- imports/ui/components/AddEntry.jsx
+++ imports/ui/components/AddEntry.jsx
@@ -5,7 +5,16 @@ export default class AddEntry extends Component{
     render(){
         return (
             <div>
-            
+                    <div className="container login-container">
+                        <div className="row">
+                            <div className="col-12 centered">
+                                <h3 className="text-center">Add Entry</h3>
+                                <form ref="addentry" className="addentry-form">
+                                    <input ref="date"/>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
             </div>
         );
     };
