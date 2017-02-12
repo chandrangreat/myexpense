96d71d8bab887a035c5fb7ce9725a9faa5d03fac
chandrangreat <chandrangreatfree@gmail.com>
2017-02-12 22:29:56 +0530

    Add Datepicker in AddEntry component

--- imports/ui/components/AddEntry.jsx
+++ imports/ui/components/AddEntry.jsx
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.css';//../../../node_modules/wingcss/dist/wing.min.css';
+
 
 
 export default class AddEntry extends Component{
+
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            startDate: moment(),
+        };
+     }
+   
+
+    handleDateChange(date){
+        this.setState({
+            startDate: date,
+        });
+    }
+
     render(){
         return (
             <div>
@@ -10,7 +30,9 @@ export default class AddEntry extends Component{
                             <div className="col-12 centered">
                                 <h3 className="text-center">Add Entry</h3>
                                 <form ref="addentry" className="addentry-form">
-                                    <input ref="date"/>
+                                    <DatePicker
+                                           selected={this.state.startDate}
+                                           onChange={this.handleDateChange.bind(this)} />
                                 </form>
                             </div>
                         </div>
